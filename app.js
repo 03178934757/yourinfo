8a92e922b448402b4119f0aa5c5960aeec7d1546
Muhammad Waleed <153723766+03178934757@users.noreply.github.com>
2024-04-23 22:28:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var yourName = prompt("Enter Your Name");
+var yourAge = prompt ("Enter Your Age");
+
+document.write (yourName+ "Ap Pichle"  + yourAge + "Saal se is duniya me gand ku mararhe hain?")
